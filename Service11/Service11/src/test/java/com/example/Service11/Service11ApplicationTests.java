0ce7cf04dbf69460db152d5b70ba1fbbe740cc1d
Gopichand Gedala <gopichandgedala@gmail.com>
2022-07-11 14:41:20 +0530

    first commit

---
+++ Service11/Service11/src/test/java/com/example/Service11/Service11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Service11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Service11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Service12/src/test/java/com/example/Service12/Service12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Service12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Service12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
